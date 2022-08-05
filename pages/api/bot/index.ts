d4b533a4970a0f2307f8e15ecd6085fedce76314
Kami-Sqmf <kamisqmf@gmail.com>
2022-08-05 16:22:15 +0800

    PreAlpha 1.6.0

--- pages/api/bot/index.ts
+++ pages/api/bot/index.ts
@@ -1,30 +1,34 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import LineBot from '../../../utils/line-bot';
+import LineBot from '../../../utils/lineBot';
+import MessageObject from '../../../utils/lineObject';
+import { LineRequest } from '../../../utils/lineType';
 
-const bot = new LineBot(
-  '1jFiXttWeXRsbc7xYIFvPsUVpOBSYMB93HDXvDAgLmybJq4+bNdYCyZf7SZIMxd6+96yaq1Re3WonVKQAz5MfaoGTQphHAzeOcaNObWxGCNrcJ6/RLir9TOOnUqi5JLwmrfN0k4XSAmN1qNl4MyJzAdB04t89/1O/w1cDnyilFU='
-);
+const dotenv = require('dotenv');
+const crypto = require("crypto");
+const bot = new LineBot(process.env.LinebotKey)
+dotenv.config();
 
-const handler = (_req: NextApiRequest, res: NextApiResponse) => {
+export default async (_req: NextApiRequest, res: NextApiResponse) => {
   try {
-    if (_req.method !== 'POST') {
-      // bot.temp()
-      res.status(404).send({ message: '請使用Line傳送資料' });
-      return;
-    } else {
-      const event = _req.body.events;
-      console.log(event);
-      bot.event(event).then(()=>{
-        res.status(200).send({ operation: 'success' })
-      }).catch((err)=>{
-        console.log(err)
-        res.status(500).json({ statusCode: 500, message: err.message })
-      })
-    }
+    // Not Post Method
+    if (_req.method != 'POST') throw '目前不支援此通道！';
+
+    // Check Reqeust From Line
+    const body = _req.body as LineRequest
+    const signature = crypto
+      .createHmac("SHA256", process.env.LineSignature)
+      .update(JSON.stringify(body))
+      .digest("base64");
+    if(_req.headers['x-line-signature'] != signature) throw "不認識的詐騙人士！";
+
+    // Handler
+    const event = body.events;
+    console.log(event);
+    await bot.event(event)
+    res.status(200).send({ operation: '操作成功' })
+
   } catch (err: any) {
     console.log(err)
     res.status(500).json({ statusCode: 500, message: err })
   }
 }
-
-export default handler
